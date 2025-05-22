3e4d6a551471a5be810c4cb4f2ad221be4b47636
Clot-Godard Kenji <kenji.clot-godard@etu.univ-amu.fr>
2025-05-22 15:44:58 +0200

    Exercice 13

--- src/main/java/fr/amu/iut/exercice13/MainPersonnes.java
+++ src/main/java/fr/amu/iut/exercice13/MainPersonnes.java
@@ -4,20 +4,44 @@ import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
-@SuppressWarnings("Duplicates")
-public class MainPersonnes  {
+public class MainPersonnes {
 
     private static ObservableList<Personne> lesPersonnes;
 
-    private static ListChangeListener<Personne> unChangementListener;
-
     public static void main(String[] args) {
 
-        lesPersonnes = FXCollections.observableArrayList();
+        lesPersonnes = FXCollections.observableArrayList(personne ->
+                new javafx.beans.Observable[]{personne.ageProperty()}
+        );
 
-//        unChangementListener = à completer
+        ListChangeListener<Personne> plusieursChangementsListener = change -> {
+            System.out.println("Début d’une série de changements :");
 
-        lesPersonnes.addListener(unChangementListener);
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    for (Personne p : change.getAddedSubList()) {
+                        System.out.println("Ajouté : " + p.getNom());
+                        p.ageProperty().addListener((obs, oldVal, newVal) ->
+                                System.out.println(p.getNom() + " a maintenant " + newVal + " ans"));
+                    }
+                }
+                if (change.wasRemoved()) {
+                    for (Personne p : change.getRemoved()) {
+                        System.out.println("Supprimé : " + p.getNom());
+                    }
+                }
+            }
+
+            System.out.println("Fin des changements.");
+        };
+
+        lesPersonnes.addListener(plusieursChangementsListener);
+
+        // Choisissez une des méthodes pour tester :
+        // question1();
+        // question2();
+        // question3();
+        // question5();
     }
 
     public static void question1() {
@@ -55,8 +79,7 @@ public class MainPersonnes  {
         Personne jacques = new Personne("Jacques", 60);
         lesPersonnes.addAll(pierre, paul, jacques);
         for (Personne p : lesPersonnes)
-            p.setAge(p.getAge()+10);
+            p.setAge(p.getAge() + 10);
         lesPersonnes.removeAll(paul, pierre);
     }
 }
-
