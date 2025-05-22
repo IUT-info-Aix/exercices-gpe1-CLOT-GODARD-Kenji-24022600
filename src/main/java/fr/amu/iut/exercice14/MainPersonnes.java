a7dbe83f1146e4afdf3acee5502a42078679d6d1
Clot-Godard Kenji <kenji.clot-godard@etu.univ-amu.fr>
2025-05-22 15:52:16 +0200

    Exercice 14

--- src/main/java/fr/amu/iut/exercice14/MainPersonnes.java
+++ src/main/java/fr/amu/iut/exercice14/MainPersonnes.java
@@ -1,4 +1,4 @@
-package fr.amu.iut.exercice4;
+package fr.amu.iut.exercice14;
 
 import javafx.beans.binding.IntegerBinding;
 import javafx.beans.property.IntegerProperty;
@@ -6,7 +6,6 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 
-@SuppressWarnings("Duplicates")
 public class MainPersonnes {
 
     private static SimpleListProperty<Personne> lesPersonnes;
@@ -14,15 +13,81 @@ public class MainPersonnes {
     private static IntegerProperty nbParisiens;
 
     private static IntegerBinding calculAgeMoyen;
-    private static IntegerBinding calculnbParisiens;
+    private static IntegerBinding calculNbParisiens;
 
     public static void main(String[] args) {
 
         lesPersonnes = new SimpleListProperty<>(FXCollections.observableArrayList());
         ageMoyen = new SimpleIntegerProperty(0);
+        nbParisiens = new SimpleIntegerProperty(0);
+
+        // BINDING POUR L'AGE MOYEN
+        calculAgeMoyen = new IntegerBinding() {
+            {
+                bind(lesPersonnes);
+                lesPersonnes.addListener((javafx.collections.ListChangeListener<Personne>) change -> {
+                    while (change.next()) {
+                        if (change.wasAdded() || change.wasRemoved()) {
+                            for (Personne p : change.getAddedSubList()) {
+                                bind(p.ageProperty());
+                                p.ageProperty().addListener((obs, oldVal, newVal) -> invalidate());
+                            }
+                            for (Personne p : change.getRemoved()) {
+                                unbind(p.ageProperty());
+                            }
+                        }
+                    }
+                    invalidate();
+                });
+            }
+
+            @Override
+            protected int computeValue() {
+                if (lesPersonnes.isEmpty()) return 0;
+                int somme = 0;
+                for (Personne p : lesPersonnes)
+                    somme += p.getAge();
+                return somme / lesPersonnes.size();
+            }
+        };
+
+        ageMoyen.bind(calculAgeMoyen);
+
+        // BINDING POUR LE NOMBRE DE PARISIENS
+        calculNbParisiens = new IntegerBinding() {
+            {
+                bind(lesPersonnes);
+                lesPersonnes.addListener((javafx.collections.ListChangeListener<Personne>) change -> {
+                    while (change.next()) {
+                        if (change.wasAdded() || change.wasRemoved()) {
+                            for (Personne p : change.getAddedSubList()) {
+                                bind(p.villeDeNaissanceProperty());
+                                p.villeDeNaissanceProperty().addListener((obs, oldVal, newVal) -> invalidate());
+                            }
+                            for (Personne p : change.getRemoved()) {
+                                unbind(p.villeDeNaissanceProperty());
+                            }
+                        }
+                    }
+                    invalidate();
+                });
+            }
+
+            @Override
+            protected int computeValue() {
+                int count = 0;
+                for (Personne p : lesPersonnes) {
+                    if (p.getVilleDeNaissance().equals("Paris"))
+                        count++;
+                }
+                return count;
+            }
+        };
+
+        nbParisiens.bind(calculNbParisiens);
 
         question1();
-//        question2();
+        question2();
     }
 
     public static void question1() {
@@ -43,15 +108,14 @@ public class MainPersonnes {
     }
 
     public static void question2() {
-        System.out.println("Il y a " + nbParisiens.getValue() + " parisiens");
+        System.out.println("Il y a " + nbParisiens.getValue() + " parisien(s)");
         lesPersonnes.get(0).setVilleDeNaissance("Marseille");
-        System.out.println("Il y a " + nbParisiens.getValue() + " parisiens");
+        System.out.println("Il y a " + nbParisiens.getValue() + " parisien(s)");
         lesPersonnes.get(1).setVilleDeNaissance("Montpellier");
-        System.out.println("Il y a " + nbParisiens.getValue() + " parisien");
+        System.out.println("Il y a " + nbParisiens.getValue() + " parisien(s)");
         for (Personne p : lesPersonnes)
             p.setVilleDeNaissance("Paris");
-        System.out.println("Il y a " + nbParisiens.getValue() + " parisiens");
+        System.out.println("Il y a " + nbParisiens.getValue() + " parisien(s)");
     }
 
 }
-
--- src/main/java/fr/amu/iut/exercice14/Personne.java
+++ src/main/java/fr/amu/iut/exercice14/Personne.java
@@ -1,4 +1,4 @@
-package fr.amu.iut.exercice4;
+package fr.amu.iut.exercice14;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -7,9 +7,9 @@ import javafx.beans.property.StringProperty;
 
 public class Personne {
 
-    private String nom;
-    private IntegerProperty age;
-    private StringProperty villeDeNaissance;
+    private final String nom;
+    private final IntegerProperty age;
+    private final StringProperty villeDeNaissance;
 
     public Personne(String nom, int age) {
         this.nom = nom;
@@ -17,11 +17,31 @@ public class Personne {
         this.villeDeNaissance = new SimpleStringProperty("Paris");
     }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public int getAge() {
+        return age.get();
+    }
+
     public void setAge(int age) {
-        this.age.setValue(age);
+        this.age.set(age);
+    }
+
+    public IntegerProperty ageProperty() {
+        return age;
+    }
+
+    public String getVilleDeNaissance() {
+        return villeDeNaissance.get();
     }
 
     public void setVilleDeNaissance(String ville) {
-        villeDeNaissance.setValue(ville);
+        villeDeNaissance.set(ville);
+    }
+
+    public StringProperty villeDeNaissanceProperty() {
+        return villeDeNaissance;
     }
 }
